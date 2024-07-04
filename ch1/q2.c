9370c17342600c1f91b0688c81b2798b9ae151c3
Kavya6045 <115174738+Kavya6045@users.noreply.github.com>
2024-07-04 18:25:40 +0530

    Add files via upload

---
+++ ch1/q2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    float base_salary, gross_salary;
+    float hra_percent, da_percent, ta_percent;
+
+    
+    printf("Enter base salary: ");
+    scanf("%f", &base_salary);
+
+   
+    printf("Enter percentage of HRA: ");
+    scanf("%f", &hra_percent);
+    printf("Enter percentage of DA: ");
+    scanf("%f", &da_percent);
+    printf("Enter percentage of TA: ");
+    scanf("%f", &ta_percent);
+
+   
+    float hra_amount = base_salary * (hra_percent / 100);
+    float da_amount = base_salary * (da_percent / 100);
+    float ta_amount = base_salary * (ta_percent / 100);
+
+   
+    gross_salary = base_salary + hra_amount + da_amount + ta_amount;
+
+    
+    printf("\nBreakdown:\n");
+    printf("Base Salary: %.2f\n", base_salary);
+    printf("HRA: %.2f\n", hra_amount);
+    printf("DA: %.2f\n", da_amount);
+    printf("TA: %.2f\n", ta_amount);
+    printf("\nGross Salary: %.2f\n", gross_salary);
+
+    return 0;
+}
+
